9fd903d8ba3c302b2dd0b4c2531bce146f823e2b
Patrick Heflin <patrickslx@yahoo.com>
2020-09-30 10:54:37 -0500

    *

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express()
 app.use(morgan('tiny'))
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.get('/videos', (req, res) => {
   const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLDU0zxRNUv8lKih2evaNl9V5wDkP7N4Ul'
@@ -36,10 +40,6 @@ function errorHandler(error, req, res, next) {
 
 app.use(notFound)
 app.use(errorHandler)
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {

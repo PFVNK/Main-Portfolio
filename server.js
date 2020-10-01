9f630e6793de89f3d8e04c18390dc5ba09f6f592
Patrick Heflin <patrickslx@yahoo.com>
2020-10-01 10:05:33 -0500

    fix fetch

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express()
 app.use(morgan('tiny'))
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/videos', (req, res) => {
   const url = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLDU0zxRNUv8lKih2evaNl9V5wDkP7N4Ul'
   fetch(`${url}&key=${process.env.GOOGLE_API_KEY}`)
@@ -34,6 +36,10 @@ function errorHandler(error, req, res, next) {
 
 app.use(notFound)
 app.use(errorHandler)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
